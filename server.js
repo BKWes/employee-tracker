08eb641a8523289e0f11e9c0a45f052172d40dfe
Weston <barnesweston@gmail.com>
2022-04-28 17:01:36 -0500

    require file dependecies, add connection call

---
+++ server.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql2');
+const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'employees'
+});
