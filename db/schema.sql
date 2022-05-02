b5e63e80e8540499ae8c3803623b8bfdf46c29ec
Weston <barnesweston@gmail.com>
2022-05-01 19:48:18 -0500

    populate departments table

--- db/schema.sql
+++ db/schema.sql
@@ -3,30 +3,30 @@ DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employees;
 
 CREATE TABLE departments (
-    id INTEGER PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
 
-CREATE TABLE roles (
-    id INT PRIMARY KEY,
-    title VARCHAR(30),
-    department_id INTEGER,
-    salary DECIMAL,
-    FOREIGN KEY (department_id) REFERENCES department(id)
-    ON DELETE SET NULL
-);
+-- CREATE TABLE roles (
+--     id INT PRIMARY KEY,
+--     title VARCHAR(30),
+--     department_name VARCHAR(30),
+--     salary DECIMAL,
+--     FOREIGN KEY (department_name) REFERENCES departments(name)
+--     ON DELETE SET NULL
+-- );
 
-CREATE TABLE employees (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    department_id INTEGER, 
-    salary DECIMAL,   
-    role_id INTEGER,
-    FOREIGN KEY (role_id) REFERENCES roles(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY (salary) REFERENCES roles(salary)
-    ON DELETE SET NULL
-);
+-- CREATE TABLE employees (
+--     id INT PRIMARY KEY,
+--     first_name VARCHAR(30),
+--     last_name VARCHAR(30),
+--     department_name VARCHAR(30), 
+--     salary DECIMAL,   
+--     role_id INTEGER,
+--     FOREIGN KEY (role_id) REFERENCES roles(id)
+--     ON DELETE SET NULL,
+--     FOREIGN KEY (department_name) REFERENCES departments(name)
+--     ON DELETE SET NULL,
+--     FOREIGN KEY (salary) REFERENCES roles(salary)
+--     ON DELETE SET NULL
+-- );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO department(name)
+INSERT INTO departments(name)
 VALUES
 ('General Merchandise'),
 ('Customer Service'),
