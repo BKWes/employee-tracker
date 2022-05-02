bf884a04541a969e2907f0d0cdc35149a900a730
Weston <barnesweston@gmail.com>
2022-05-02 18:58:31 -0500

    add database to env file

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PW,
-    database: 'employees'
+    database: process.env.DB
 });
 
 module.exports = db;
