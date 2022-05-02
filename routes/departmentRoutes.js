4df55af647b4d61da7f68f7fc21f683191adf82d
Weston <barnesweston@gmail.com>
2022-05-01 20:34:58 -0500

    add department api routes: get all, get one, and post

---
+++ routes/departmentRoutes.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db/connection');
+
+// GET all departments
+router.get('/departments', (req, res) => {
+    const sql = `SELECT * FROM departments`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// GET ONE department by ID
+router.get('/department/:id', (req, res) => {
+    const sql = `SELECT * FROM departments WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// POST department
+router.post('/department', ({ body }, res) => {
+    const sql = `INSERT INTO departments (name)
+                VALUES (?)`;
+    db.query(sql, [body.name], (err, result) => {
+        if (err) {
+            res.status(400)({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const apiRoutes = require('./routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: false} ));
+app.use(express.json());
 
+app.use('/api', apiRoutes);
 
 // inquirer options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 
